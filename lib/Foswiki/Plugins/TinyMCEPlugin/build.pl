8b7a3b56b6473971c567e8b4f532de4748de13af
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-09-14 21:31:26 +0000

    Item2070: Merge new TMCE/Wysiwyg pair over to 1.0.7 And finally copy over TinyMCE from trunk
    git-svn-id: http://svn.foswiki.org/branches/Release01x00/TinyMCEPlugin@4922 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TinyMCEPlugin/build.pl
+++ lib/Foswiki/Plugins/TinyMCEPlugin/build.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl -w
 
+use strict;
+
 BEGIN {
   foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
     unshift @INC, $pc;
@@ -8,7 +10,7 @@ BEGIN {
 
 use Foswiki::Contrib::Build;
 
-$build = new Foswiki::Contrib::Build( "TinyMCEPlugin" );
+my $build = new Foswiki::Contrib::Build( "TinyMCEPlugin" );
 $build->build($build->{target});
 
 
